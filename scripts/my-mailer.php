4b1978f63ad142dd6eca69d509c32251c9469f8c
Daniel <thecodedaniel@gmail.com>
2023-10-25 16:00:45 +0100

    new

--- scripts/my-mailer.php
+++ scripts/my-mailer.php
@@ -5,28 +5,35 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$to = 'thecodedaniel@gmail.com';
+if (isset($emaill) && isset($name) && isset($message)) {
+    $to = 'danielainoko402@gmail.com';
 
-$subject = 'New message from ' . $name;
-$emailBody = "Name: $name\n";
-$emailBody .= "Email: $email\n";
-$emailBody .= "Message: $message\n";
+    $subject = 'New message from ' . $name;
+    $emailBody = "Name: $name\n";
+    $emailBody .= "Email: $email\n";
+    $emailBody .= "Message: $message\n";
 
-$headers = array(
-    'From' => $email,
-    'Reply-To' => $email,
-    'X-Mailer' => 'PHP/' . phpversion(),
-    'X-Priority' => '1'
-);
+    $headers = array(
+        'From' => $email,
+        'Reply-To' => $email,
+        'X-Mailer' => 'PHP/' . phpversion(),
+        'X-Priority' => '2'
+    );
 
-$headerLines = '';
-foreach ($headers as $key => $value) {
-    $headerLines .= $key . ': ' . $value . "\r\n";
-}
+    $headerLines = '';
+    foreach ($headers as $key => $value) {
+        $headerLines .= $key . ': ' . $value . "\r\n";
+    }
+    $mail = mail($to, $subject, $emailBody, $headerLines);
+    // Send the email
+    if ($mail) {
+        echo $mail;
+        echo 'email-sent';
+    } else {
 
-// Send the email
-if (mail($to, $subject, $emailBody, $headerLines)) {
-    echo 'email-sent';
+        echo $mail;
+        echo 'email-failed';
+    }
 } else {
-    echo 'email-failed';
+    echo "Incomplete fields";
 }
